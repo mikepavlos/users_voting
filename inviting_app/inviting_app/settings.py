983ccf2ffeee1428ed6d3da1cfc2a44549b5d039
mikepavlos <pavlovichmihaylovich@yandex.ru>
2023-08-25 13:17:55 +0300

    Включение файла переменных окружения; Readme.md

--- inviting_app/inviting_app/settings.py
+++ inviting_app/inviting_app/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kzt*=1q957j92mnx2z-wgfn(sq-)728q-wd52w6fpe7@95py!t'
+SECRET_KEY = os.getenv('SECRET_KEY', default='top_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
